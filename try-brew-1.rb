2afdb31ed07a7646efcd993c8b3eac20d926eaa3
Jay Sullivan <jaythomassullivan@gmail.com>
2020-07-12 14:08:19 -0400

    add homebrew formula

---
+++ try-brew-1.rb
@@ -0,0 +1,14 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class TryBrew1 < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/notfed/try-brew-1/archive/v0.0.1.tar.gz"
+  sha256 "c69dc4ee1084ace182eca0e1391e2c66c9aa3f4e1441c154ccb04ee7963f214d"
+
+  def install
+      bin.install "try-brew-1"
+  end
+
+end
